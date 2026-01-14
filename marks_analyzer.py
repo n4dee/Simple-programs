5c66de9975dd467d2b344eaad8fcdce38e1902fc
n4dee <nadeemohamed07@gmail.com>
2026-01-14 21:59:58 +0530

    marks_analyzer.py

--- marks_analyzer.py
+++ marks_analyzer.py
@@ -7,10 +7,9 @@ marks = []
 for i in range(5):
     mark = int(input(f"Enter mark for subject {i+1}: "))
     marks.append(mark)
-
+    
 total = sum(marks)
 average = total / len(marks)
-
 print("\n--- Result ---")
 print("Student Name:", name)
 print("Total Marks:", total)
